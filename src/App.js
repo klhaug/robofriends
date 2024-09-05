1b06848e900d52b060c22f53b23f91d170ed2fc1
klhaug <kristian@snougle.no>
2024-09-05 06:56:32 +0200

    starting working with state

--- src/App.js
+++ src/App.js
@@ -2,15 +2,31 @@ import React from "react";
 import CardList from './CardList';
 import { robots } from './robots';
 import SearchBox from './SearchBox';
+import { render } from "@testing-library/react";
 
-const App = () => {
+
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+
+    onSearchChange(event) {
+        console.log(event);
+    }
+
+    render() {
     return (
         <div className="tc">
             <h1>Robofriends</h1>
-            <SearchBox />
-            <CardList robots = {robots}/>
+            <SearchBox searchChange ={this.onSearchChange} />
+            <CardList robots = {this.state.robots}/>
         </div>
-    )
+        );
+    }
 }
 
 
