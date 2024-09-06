79d2332908d8858ab3e97a42f0d0eecd2b91fbc2
klhaug <kristian@snougle.no>
2024-09-06 06:05:50 +0200

    trying fetch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import CardList from './CardList';
-import { robots } from './robots';
+// import { robots } from './robots';
 import SearchBox from './SearchBox';
 import { render } from "@testing-library/react";
 
@@ -9,9 +9,15 @@ class App extends React.Component {
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         } 
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response =>  response.json())
+            .then(users => this.setState({ robots: users}));
     }
 
     onSearchChange = (event) => {
